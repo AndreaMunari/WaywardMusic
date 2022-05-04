724c63ddd5d287bda87a9d40484e8f06b2943cf5
PieroZamboni <103607145+PieroZamb@users.noreply.github.com>
2022-05-04 15:19:16 +0200

    Modal+Less

--- function.js
+++ function.js
@@ -1,6 +1,13 @@
+//Scroll
 $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
   });
+
+  //Modal
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  });
+
