6500ae99b200f57592b2134808604c7f3be4404e
PieroZamboni <103607145+PieroZamb@users.noreply.github.com>
2022-05-05 18:04:12 +0200

    slidebar

--- function.js
+++ function.js
@@ -32,3 +32,15 @@ function check_for_fade() {
  
     });
 };;
+
+
+//Preloader
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".pre-loader").length > 0)
+        {
+            $(".pre-loader").fadeOut("slow");
+        }
+    });
+})(jQuery)
